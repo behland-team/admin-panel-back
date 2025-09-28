0325d7b4f55733d4d1238c8236ec8d7506a5a0a6
parham <parhamhassanzade.p@gmail.com>
2025-09-28 22:39:28 +0200

    add roadmap app and model

---
+++ admin_panel/roadmap/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class RoadmapSection(models.Model):
+    title=models.CharField(max_length=200)
+    order=models.PositiveBigIntegerField()
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return self.title
+
+
+
+class RoadmapItem(models.Model):
+    section=models.ForeignKey(RoadmapSection,on_delete=models.CASCADE,related_name="items")
+    description=models.TextField()
+    order=models.IntegerField()
+    is_completed=models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return self.desc[:50]
