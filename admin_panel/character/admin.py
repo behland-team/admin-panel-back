e9ced3855e9ef5236079dc20be777517d0e82b1f
parham <parhamhassanzade.p@gmail.com>
2025-10-01 20:31:53 +0200

    add character and contact model in admin

--- admin_panel/character/admin.py
+++ admin_panel/character/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from character.models import character
 
 # Register your models here.
+admin.site.register(character)
--- admin_panel/contact/admin.py
+++ admin_panel/contact/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from contact.models import ContactMessage
 # Register your models here.
+
+admin.site.register(ContactMessage)
