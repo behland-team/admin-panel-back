1e245c052b76bf822d383d5374ed197553b0b87c
parham <parhamhassanzade.p@gmail.com>
2025-10-26 13:50:04 +0100

    change redocs

--- admin_panel/admin_panel/urls.py
+++ admin_panel/admin_panel/urls.py
@@ -34,6 +34,6 @@ urlpatterns = [
         path("blog/", include("blog.urls")),  
           path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     ])),
 ]
