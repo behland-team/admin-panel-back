a3016fc08b4caa0b65143c069ef2b12ed06a8836
parham <parhamhassanzade.p@gmail.com>
2025-11-16 14:38:35 +0100

    change slug

--- admin_panel/blog/models.py
+++ admin_panel/blog/models.py
@@ -66,7 +66,14 @@ class Post(TimeStampedModel):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+            while Post.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+
         super().save(*args, **kwargs)
 
     @property
